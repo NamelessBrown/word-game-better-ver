1d36e7696a4177c2f7da9e11c521e8ae25a878bb
Nameless <tyrebrown873@gmail.com>
2020-05-27 06:27:48 -0400

    Add project files.

---
+++ word game better ver/word game better ver.cpp
@@ -0,0 +1,9 @@
+// word game better ver.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
